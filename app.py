e49484ec81cfcfec07b811c6f43311ff57cf935c
tkaid <104912335+tkaid@users.noreply.github.com>
2022-09-18 17:49:21 +0100

    New app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index ():
-    return render_template("Index.html")
+@app.route('/Index')
+def home():
+    return render_template('Index.html')
+
+@app.route('/Data/')
+def about():
+    return render_template('Data.html')
+
+@app.route('/Graph/')
+def about():
+    return render_template('Graph.html')
+
+@app.route('/Map/')
+def about():
+    return render_template('Map.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
