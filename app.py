f3d84ec2e2bfedeb3e6acaf8e5b87d03cdb97182
tkaid <104912335+tkaid@users.noreply.github.com>
2022-09-18 20:01:49 +0100

    app.py v6

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
+from flask import Flask, redner_template 
 
 @app.route('/')
 def Index():
-    return render_template('Index.html')
+    return redner_template('Index.html')
 
-@app.route('/')
-def Data():
-    return render_template('Data.html')
-
-@app.route('/Graph/')
+@app.route('/Graph')
 def Graph():
-    return render_template('Graph.html')
+    return redner_template('Graph.html')
 
-@app.route('/Map/')
+@app.route('/Map')
 def Map():
-    return render_template('Map.html')
+    return redner_template('Map.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/Data')
+def Data():
+    return redner_template('Data.html')
+
+    if __name__ == '__main__':
+        app.run(debug=True)
