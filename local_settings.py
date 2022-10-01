2cfe3e085b4fbd89e6a34c04bc562d327a4377ec
tkaid <104912335+tkaid@users.noreply.github.com>
2022-10-01 18:38:30 +0100

    new JS

---
+++ local_settings.py
@@ -0,0 +1,6 @@
+postgresql = {'pguser':'postgres',
+              'pgpasswd':'1234',
+              'pghost':'localhost',
+              'pgport': 5432,
+              'pgdb':'Selling_Birmingham'
+}
