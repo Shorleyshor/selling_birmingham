ae4d8beb75ec353588dbc0cc18fa7a5d0541b299
tkaid <104912335+tkaid@users.noreply.github.com>
2022-10-02 01:36:15 +0100

    commit

--- BedroomsPriceScatter.js
+++ BedroomsPriceScatter.js
@@ -1,20 +1,20 @@
-console.log(data)
+console.log(data);
 
-let trace1 = {
-  x: [],
-  y: [],
+let trace1 = 
+{
+  x: [row.Price],
+  y: [row.Bedrooms],
   mode: "lines"
 };
 
-let traceData = [trace1];
+let traceData = 
+[
+  trace1
+];
 
-data.forEach(function(val) {
-  trace1.x.push(val["Price"]);
-  trace1.y.push(val["Bedrooms"]);
-
-let layout = {
-title: "Bedroom Vs Price Sactter Graph" } 
+let layout = 
+{
+title: "Bedroom Vs Price Sactter Graph" 
+};
   
-});
-
-Plotly.newPlot('Bedroom Vs Price Sactter Graph', traceData, layout);
+Plotly.newPlot("BedroomsPriceScatter", traceData, layout);
